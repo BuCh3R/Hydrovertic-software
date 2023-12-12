2bd9ed536fb3470384661cf5b03055aceec9b3b5
Egil W <egil.wendsjo@gmail.com>
2023-12-12 19:00:57 +0100

    Minor fixes

--- theme.js
+++ theme.js
@@ -1,41 +1,24 @@
-let darkTheme;
+document.getElementById('theme-button').addEventListener('click', () => { switchTheme() });
+
+let darkTheme = 0;
 
 function switchTheme() {
     if (darkTheme) {
-        document.documentElement.style.setProperty("--main-bg-color", "#1f1f1f");
-        document.documentElement.style.setProperty("--secondary-bg-color", "#171717");
-        document.documentElement.style.setProperty("--primary-color", "#008559");
-        document.documentElement.style.setProperty("--accent-color", "#3f3f3f");
-        document.documentElement.style.setProperty("--text-color", "#ffffff");
-        document.documentElement.style.setProperty("--secondary-text-color", "#bfbfbf");
-    }
-    else {
         document.documentElement.style.setProperty("--main-bg-color", "#efefef");
         document.documentElement.style.setProperty("--secondary-bg-color", "#ffffff");
         document.documentElement.style.setProperty("--primary-color", "#008559");
         document.documentElement.style.setProperty("--accent-color", "#dfdfdf");
         document.documentElement.style.setProperty("--text-color", "#3f3f3f");
         document.documentElement.style.setProperty("--secondary-text-color", "#7f7f7f");
+        darkTheme = 0;
+    }
+    else {
+        document.documentElement.style.setProperty("--main-bg-color", "#1f1f1f");
+        document.documentElement.style.setProperty("--secondary-bg-color", "#171717");
+        document.documentElement.style.setProperty("--primary-color", "#008559");
+        document.documentElement.style.setProperty("--accent-color", "#3f3f3f");
+        document.documentElement.style.setProperty("--text-color", "#ffffff");
+        document.documentElement.style.setProperty("--secondary-text-color", "#bfbfbf");
+        darkTheme = 1;
     }
 }
-
-// function switchTheme() {
-//     if (darkTheme) {
-//         sheet.insertRule(":root{--main-bg-color:#1f1f1f}");
-//         sheet.insertRule(":root{--secondary-bg-color:#171717}");
-//         sheet.insertRule(":root{--primary-color:#008559}");
-//         sheet.insertRule(":root{--accent-color:#3f3f3f}");
-//         sheet.insertRule(":root{--text-color:#ffffff}");
-//         sheet.insertRule(":root{--secondary-text-color:#bfbfbf}");
-//     }
-//     else {
-//         sheet.insertRule(":root{--main-bg-color:#f7f7f7}");
-//         sheet.insertRule(":root{--secondary-bg-color:#ffffff}");
-//         sheet.insertRule(":root{--primary-color:#008559}");
-//         sheet.insertRule(":root{--accent-color:#dfdfdf}");
-//         sheet.insertRule(":root{--text-color:#3f3f3f}");
-//         sheet.insertRule(":root{--secondary-text-color:#7f7f7f}");
-//     }
-// }
-
-switchTheme();
